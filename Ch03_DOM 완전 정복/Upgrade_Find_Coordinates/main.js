9cb72bcc26f29f5b009ce1e7c2f8e86a20c8a1cf
KwonEna <eeena214@gmail.com>
2021-09-06 21:04:30 +0900

    실습6. 좌표 실습 성능 개선

---
+++ Ch03_DOM 완전 정복/Upgrade_Find_Coordinates/main.js
@@ -0,0 +1,24 @@
+const hor = document.querySelector(".horozontal");
+const ver = document.querySelector(".vertical");
+const target = document.querySelector(".target");
+const tag = document.querySelector(".tag");
+
+addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
+
+  document.addEventListener("mousemove", (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    console.log(`${x} ${y}`);
+
+    ver.style.transform = `translateX(${x}px)`;
+    hor.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+    tag.innerHTML = `${x}px, ${y}px`;
+  });
+});
