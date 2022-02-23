9407ae7f6c599e2f095932e9b4d71bc0f4a33135
KwonEna <eeena214@gmail.com>
2022-02-23 23:59:10 +0900

    Refactor : js 파일 분리 후 popUp 클래스 생성

--- Ch07_실전 게임 만들기/src/main.js
+++ Ch07_실전 게임 만들기/src/main.js
@@ -1,4 +1,5 @@
 "use strict";
+import PopUp from "./popup.js";
 
 const CARROT_SIZE = 80;
 const CARROT_COUNT = 5;
@@ -11,10 +12,6 @@ const gameBtn = document.querySelector(".game_button");
 const gameTimer = document.querySelector(".game_timer");
 const gameScore = document.querySelector(".game_score");
 
-const popUp = document.querySelector(".pop-up");
-const popUpText = document.querySelector(".pop-up_message");
-const popUpRefresh = document.querySelector(".pop-up_refresh");
-
 const carrotSound = new Audio("./sound/carrot_pull.mp3");
 const alertSound = new Audio("./sound/alert.wav");
 const bgSound = new Audio("./sound/bg.mp3");
@@ -25,6 +22,10 @@ let started = false;
 let score = 0;
 let timer = undefined;
 
+const gameFinishBanner = new PopUp();
+gameFinishBanner.setEventListener(() => {
+  startGame();
+});
 field.addEventListener("click", onFiledClick);
 
 gameBtn.addEventListener("click", () => {
@@ -33,11 +34,6 @@ gameBtn.addEventListener("click", () => {
   } else {
     startGame();
   }
-});
-
-popUpRefresh.addEventListener("click", () => {
-  startGame();
-  hidePopUp();
 });
 
 function startGame() {
@@ -53,7 +49,7 @@ function stopGame() {
   started = false;
   stopGameTimer();
   hideGameButton();
-  showPopUpWithText("REPLAY❓");
+  gameFinishBanner.showWithText("REPLAY❓");
   playSound(alertSound);
   stopSound(bgSound);
 }
@@ -68,7 +64,7 @@ function finishGame(win) {
   }
   stopGameTimer();
   stopSound(bgSound);
-  showPopUpWithText(win ? "YOU WON🎉" : "YOU LOST💣");
+  gameFinishBanner.showWithText(win ? "YOU WON🎉" : "YOU LOST💣");
 }
 
 function showStopButton() {
@@ -110,15 +106,6 @@ function updateTimerText(time) {
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
   gameTimer.innerText = `${minutes}:${seconds}`;
-}
-
-function showPopUpWithText(text) {
-  popUpText.innerText = text;
-  popUp.classList.remove("pop-up_hide");
-}
-
-function hidePopUp() {
-  popUp.classList.add("pop-up_hide");
 }
 
 function initGame() {
