33ec8e2be4326b95786ca092f49d4f597ced94f1
KwonEna <eeena214@gmail.com>
2022-03-07 21:30:44 +0900

    Fix : 중복 함수 제거 와 타입 보장을 위한 객체 동결

--- Ch07_실전 게임 만들기/src/game.js
+++ Ch07_실전 게임 만들기/src/game.js
@@ -1,4 +1,4 @@
-import Field from "./field.js";
+import { Field, ItemType } from "./field.js";
 import * as sound from "./sound.js";
 
 // 오타를 방지해 실수를 덜어줄 수 있음
@@ -46,7 +46,7 @@ class Game {
     this.gameBtn = document.querySelector(".game_button");
     this.gameBtn.addEventListener("click", () => {
       if (this.started) {
-        this.stop();
+        this.stop(Reason.cancel);
       } else {
         this.start();
       }
@@ -73,26 +73,12 @@ class Game {
     sound.playBackground();
   }
 
-  stop() {
+  stop(reason) {
     this.started = false;
     this.stopGameTimer();
     this.hideGameButton();
-    sound.playAlert();
     sound.stopBackground();
-    this.onGameStop && this.onGameStop(Reason.cancel);
-  }
-
-  finish(win) {
-    this.started = false;
-    this.hideGameButton();
-    if (win) {
-      sound.playWin();
-    } else {
-      sound.playBug();
-    }
-    this.stopGameTimer();
-    sound.stopBackground();
-    this.onGameStop && this.onGameStop(win ? Reason.win : Reason.lose);
+    this.onGameStop && this.onGameStop(reason);
   }
 
   // this 정보에 접근할 때 arrow
@@ -101,16 +87,16 @@ class Game {
     if (!this.started) {
       return;
     }
-    if (item === "carrot") {
+    if (item === ItemType.carrot) {
       // 당근!!
       this.score++;
       this.updateScoreBoard();
       if (this.score === this.carrotCount) {
-        this.finish(true);
+        this.stop(Reason.win);
       }
-    } else if (item === "bug") {
+    } else if (item === ItemType.bug) {
       // 벌레!!
-      this.finish(false);
+      this.stop(Reason.lose);
     }
   };
 
@@ -136,7 +122,7 @@ class Game {
     this.timer = setInterval(() => {
       if (remainingTimeSec <= 0) {
         clearInterval(this.timer);
-        this.finish(this.carrotCount === this.score);
+        this.stop(this.carrotCount === this.score ? Reason.win : Reason.lose);
         return;
       }
       this.updateTimerText(--remainingTimeSec);
