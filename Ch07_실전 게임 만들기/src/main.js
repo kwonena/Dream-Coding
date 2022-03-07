38a26f3b96a7b1d5156799f798da397c46062038
KwonEna <eeena214@gmail.com>
2022-03-07 21:27:30 +0900

    Fix : JavaScript 타입 보장을 위해 객체 동결

--- Ch07_실전 게임 만들기/src/main.js
+++ Ch07_실전 게임 만들기/src/main.js
@@ -1,6 +1,7 @@
 "use strict";
 import PopUp from "./popup.js";
 import { GameBuilder, Reason } from "./game.js";
+import * as sound from "./sound.js";
 
 const gameFinishBanner = new PopUp();
 const game = new GameBuilder()
@@ -15,12 +16,15 @@ game.setGameStopListener((reason) => {
   switch (reason) {
     case Reason.cancel:
       message = "Replay❓";
+      sound.playAlert();
       break;
     case Reason.win:
       message = "YOU WON🎉";
+      sound.playWin();
       break;
     case Reason.lose:
       message = "YOU LOST💩";
+      sound.playBug();
       break;
     default:
       throw new Error("not valid reason");
